c4d0e1e0de3210986f715e5673a8c74f9f6275fa
JaphPaolo <japhet.nascimento@gmail.com>
2019-02-15 20:23:43 -0200

    Some changes and add some files

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+   /* Variables */ 
+    var loJa = "";
+    var deVice = "";
+    const atuBu = document.querySelector("#butAtua");
+    const reaPr = document.querySelector("#butProg");
+    const ediPr = document.querySelector("#butEditp");
+    const mainDiv = document.querySelector(".row");
+    const headBann = document.querySelector("#HagaUm");
+
+    /* Events */ 
+    atuBu.addEventListener("click", function(){
+        loadAtuOld();
+    });
+    reaPr.addEventListener("click", function(){
+        loadProgBann();
+    });
+    ediPr.addEventListener("click", function(){
+        loadEditProg();
+    });
+    headBann.addEventListener("click", function(){
+        goHome();
+    });   
+    
+
+    /* Functions to load prp pages */
+    function loadAtuOld(){
+        mainDiv.innerHTML = infoStores.atuForma;
+        var storEso = document.querySelector("#storeso");
+        var deviCeso = document.querySelector("#deviceso");
+        var bannereSo = document.querySelector("#bannereseso");
+        storEso.addEventListener("change", function(){ enabFie() });
+        deviCeso.addEventListener("change", function(){ enabFie() });        
+    }
+    function loadProgBann(){
+        alert("tst"); //ainda será feito.
+    }
+    function loadEditProg(){
+        alert("tst"); //ainda será feito
+    }
+    function goHome(){
+        location.reload();
+    }
+
+    function enabFie(){
+        if (storeso.value !== "SELECIONELOJA" && deviceso.value === "SELECIONEDEVICE") {
+            deviceso.disabled = false;
+            storeso.disabled = true;
+        }
+        if (deviceso.value !== "SELECIONEDEVICE"){
+            deviceso.disabled = true;
+            bannereseso.disabled = false;
+            defineFormato();
+        }        
+    }    
+
+    function defineFormato() {
+        if (storeso.value === "netshoes" && deviceso.value === "desktop") {bannereseso.innerHTML = infoStores.netshoes.banners.desktop.bnForm}
+        if (storeso.value === "netshoes" && deviceso.value === "mobile") {bannereseso.innerHTML = infoStores.netshoes.banners.mobile.bnForm}
+        if (storeso.value === "netshoes" && deviceso.value === "app") {bannereseso.innerHTML = infoStores.netshoes.banners.app.bnForm}
+        if (storeso.value === "netshoesar" && deviceso.value === "desktop") {bannereseso.innerHTML = infoStores.netshoesar.banners.desktop.bnForm}
+        if (storeso.value === "netshoesar" && deviceso.value === "mobile") {bannereseso.innerHTML = infoStores.netshoesar.banners.mobile.bnForm}
+        if (storeso.value === "netshoesar" && deviceso.value === "app") {bannereseso.innerHTML = infoStores.netshoesar.banners.app.bnForm}
+        if (storeso.value === "netshoesmx" && deviceso.value === "desktop") {bannereseso.innerHTML = infoStores.netshoesmx.banners.desktop.bnForm}
+        if (storeso.value === "netshoesmx" && deviceso.value === "mobile") {bannereseso.innerHTML = infoStores.netshoesmx.banners.mobile.bnForm}
+        if (storeso.value === "netshoesmx" && deviceso.value === "app") {bannereseso.innerHTML = infoStores.netshoesmx.banners.app.bnForm}
+        if (storeso.value === "zattini" && deviceso.value === "desktop") {bannereseso.innerHTML = infoStores.zattini.banners.desktop.bnForm}
+        if (storeso.value === "zattini" && deviceso.value === "mobile") {bannereseso.innerHTML = infoStores.zattini.banners.mobile.bnForm}
+        if (storeso.value === "zattini" && deviceso.value === "app") {bannereseso.innerHTML = infoStores.zattini.banners.app.bnForm}
+        if (storeso.value === "shoestock" && deviceso.value === "desktop") {bannereseso.innerHTML = infoStores.shoestock.banners.desktop.bnForm}
+        if (storeso.value === "shoestock" && deviceso.value === "mobile") {bannereseso.innerHTML = infoStores.shoestock.banners.demobilesktop.bnForm}
+        if (storeso.value === "shoestock" && deviceso.value === "app") {bannereseso.innerHTML = infoStores.shoestock.banners.app.bnForm}
+    }
+    
+
---
+++ js/stores.js
@@ -0,0 +1,51 @@
+infoStores = {
+    "atuForma": "<div class='col-sm'> <div class='item'> <div class='selectdiv'> <select class='selecitem' name='stores' id='storeso'> <option value='SELECIONELOJA'>LOJA</option> <option value='netshoes'>NETSHOES</option> <option value='netshoesar'>NETSHOES AR</option> <option value='netshoesmx'>NETSHOES MX</option> <option value='zattini'>ZATTINI</option> <option value='shoestock'>SHOESTOCK</option> </select> </div></div></div><div class='col-sm'> <div class='item'> <div class='selectdiv'> <select class='selecitem' name='devices' id='deviceso' disabled> <option value='SELECIONEDEVICE'>DEVICE</option> <option value='desktop'>DESKTOP</option> <option value='mobile'>MOBILE</option> <option value='app'>APP</option> </select> </div></div></div><div class='col-sm'> <div class='item'> <div class='selectdiv'> <select class='selecitem' name='bannereses' id='bannereseso' disabled> <option value='FORMATO'>FORMATO</option> </select> </div></div></div>",
+    "netshoes": {
+        "storeid": "L_NETSHOES",        
+        "banners": {
+            "desktop": {
+                "bnForm":"<option value='FORMATO'>FORMATO</option> <option value='full1'>FULL1</option> <option value='full2'>FULL2</option> <option value='full3'>FULL3</option> <option value='deal1'>DEAL1</option> <option value='deal2'>DEAL2</option> <option value='deal3'>DEAL3</option> <option value='deal4'>DEAL4</option> <option value='trio1'>TRIO2</option> <option value='trio2'>TRIO3</option> <option value='trio3'>TRIO4</option> <option value='categorias'>CATEGORIAS</option> <option value='trio4'>TRIO4</option> <option value='trio5'>TRIO5</option> <option value='trio6'>TRIO6</option> <option value='marcas'>MARCAS</option> <option value='horizontal'>HORIZONTAL</option> <option value='trio7'>TRIO7</option> <option value='trio8'>TRIO8</option> <option value='trio9'>TRIO9</option> <option value='trjhome'>TARJA TOP HOME</option> <option value='trjlista'>TARJA TOP LISTA</option> <option value='trjpdp'>TARJA TOP PDP</option> <option value='headerb'>HEADER BANNER</option>",
+            },
+            "mobile": {},
+            "app": {}
+        }
+    },
+    "zattini": {
+        "storeid": "L_ZATTINI",
+        "bnForm":{},
+        "banners": {
+            "desktop": {
+                "bnForm":"<option value='FORMATO'>FORMATO</option> <option value='full1'>FULL1</option> <option value='full2'>FULL2</option> <option value='full3'>FULL3</option> <option value='deal1'>DEAL1</option> <option value='deal2'>DEAL2</option> <option value='deal3'>DEAL3</option> <option value='deal4'>DEAL4</option> <option value='trio1'>TRIO2</option> <option value='trio2'>TRIO3</option> <option value='trio3'>TRIO4</option> <option value='categorias'>CATEGORIAS</option> <option value='trio4'>TRIO4</option> <option value='trio5'>TRIO5</option> <option value='trio6'>TRIO6</option> <option value='marcas'>MARCAS</option> <option value='horizontal'>HORIZONTAL</option> <option value='trio7'>TRIO7</option> <option value='trio8'>TRIO8</option> <option value='trio9'>TRIO9</option> <option value='trjhome'>TARJA TOP HOME</option> <option value='trjlista'>TARJA TOP LISTA</option> <option value='trjpdp'>TARJA TOP PDP</option> <option value='headerb'>HEADER BANNER</option>",
+            },
+            "mobile": {},
+            "app": {}
+        }
+    },
+    "netshoesar": {
+        "storeid": "L_NETSHOESAR",
+        "bnForm":{},
+        "banners": {
+            "desktop": {},
+            "mobile": {},
+            "app": {}
+        }
+    },
+    "netshoesmx": {
+        "storeid": "L_NETSHOESMX",
+        "bnForm":{},
+        "banners": {
+            "desktop": {},
+            "mobile": {},
+            "app": {}
+        }
+    },
+    "shoestock": {
+        "storeid": "L_SHOESTOCK",
+        "bnForm":{},
+        "banners": {
+            "desktop": {},
+            "mobile": {},
+            "app": {}
+        }
+    }
+}
